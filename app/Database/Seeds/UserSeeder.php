7f9c2564ec246253afc2ce6e9ba83990546477f0
@hamidrahcom <hamidrah.com@gmail.com>
2021-01-29 04:47:44 +0330

    Create UserSeeder.php

---
+++ app/Database/Seeds/UserSeeder.php
@@ -0,0 +1,17 @@
+<?php namespace App\Database\Seeds;
+
+use App\Repositories\UserRepository;
+
+class UserSeeder extends \CodeIgniter\Database\Seeder
+{
+    public function run()
+    {
+        UserRepository::create([
+                        'name' => 'مدیر',
+                        'family' => 'کل',
+            'email' => 'admin@admin.com',
+            'password' => '123456',
+            'role' => 'admin'
+        ]);
+    }
+}
