39531d22ca0af34846921a4123e462f25ab270a0
@hamidrahcom <hamidrah.com@gmail.com>
2021-01-29 04:56:36 +0330

    Add files via upload

---
+++ app/Language/fa/Core.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file is part of the CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+// Core language settings
+return [
+	'copyError'                    => 'مشکلی در هنگام جایگزینی فایل بوجود آمده است. لطفا اطمینان حاصل کنید که شاخه‌ی فایل قابل نوشتن است.',
+	'enabledZlibOutputCompression' => 'Your zlib.output_compression ini directive is turned on. This will not work well with output buffers.',
+	'invalidFile'                  => 'فایل نامعتبر: {0}',
+	'missingExtension'             => 'افزونه‌ی {0} بارگزاری نشده است.',
+	'noHandlers'                   => '{0} باید حداقل یک Handler داشته باشد.',
+];
